2e60eb82df455283de5c629b0961af6634a32283
dennisfigueroa <denniskfig@gmail.com>
2022-02-13 23:42:21 -0500

    Updated with posters

--- Laravel-Frontend/src/components/navbar.js
+++ Laravel-Frontend/src/components/navbar.js
@@ -2,22 +2,22 @@ import React from 'react'
 
 function navbar() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style={{height:"5rem", padding:"1rem 1.5rem"}}>
-            <a class="navbar-brand" href="#" style={{fontSize: "1.5rem"}} >TV Show Application</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" d   ata-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{ padding:"1rem 1.5rem"}}>
+            <a className="navbar-brand" href="#" >TV Show Application</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav" style={{justifyContent:"flex-end"}}>
+            <div className="collapse navbar-collapse" id="navbarNav" style={{justifyContent:"flex-end"}}>
                 
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#" style={{marginRight: "100px", fontSize:"1.4rem"}}>Home <span class="sr-only">(current)</span></a>
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#" style={{marginRight: "100px"}}>Home <span className="sr-only">(current)</span></a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" style={{marginRight: "100px", fontSize:"1.4rem"}}>Login</a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#" style={{marginRight: "100px"}}>Login</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" style={{marginRight: "100px", fontSize:"1.4rem"}}>Register</a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#" style={{marginRight: "100px"}}>Register</a>
                     </li>
                 </ul>
             </div>
--- Laravel-Frontend/src/pages/Home.js
+++ Laravel-Frontend/src/pages/Home.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/navbar'; 
 function Home() {
+    const apiCall = "https://api.themoviedb.org/3/discover/tv?api_key=04c35731a5ee918f014970082a0088b1&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false"; 
+    const [tvShows, setTvShows] = useState(null) 
+
+    
+    useEffect(() =>  {
+        fetch(apiCall)
+          .then(response => response.json()
+            .then (json => {setTvShows(json.results)})
+        )
+        console.log(tvShows);
+      },[] )   
+    
+
+
     return (
-        <Navbar />
+        <>
+            <Navbar />
+         
+                {tvShows && tvShows.map(tvShow => (
+                    <div className="movie">
+                        <img src={`https://image.tmdb.org/t/p/w500/` + tvShow.poster_path} />
+                        <h6>{tvShow.name}</h6>
+
+
+                    </div>
+                ))}
+                 </>
     )
 }
 
